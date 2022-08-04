d41e5370d5f66980466f98532f6f899250a6d98b
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-04 23:13:42 +0930

    cheer

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 bool is_palindrome(int integers[], int length);
 
+int sum_array_elements(int integers[], int length);
+
 int sum_if_palindrome(int integers[], int length)
 {
     if(length <= 0)
