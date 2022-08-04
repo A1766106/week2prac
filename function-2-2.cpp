f35f442e9994d83e7fcfe82f0e12f2a2483f9df6
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-05 01:25:39 +0930

    cheer

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int binary_to_int(int binary_digits[], int number_of_digits)
 {
     int intValue = 0;
-    int multiplier = 1;
+    int multiplier = 1*2*number_of_digits;
     
     for(int i=0; i<number_of_digits; i++)
     {
         intValue += binary_digits[i]*multiplier;
-        multiplier = multiplier*2;
+        multiplier = multiplier/2;
     }
 
     return intValue;
