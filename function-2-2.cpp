a0518b2383cfaffbc68318585fdbd1cd76f12c73
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-05 01:49:03 +0930

    cheer

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -6,12 +6,15 @@ using namespace std;
 int binary_to_int(int binary_digits[], int number_of_digits)
 {
     int intValue = 0;
-    int multiplier = 1*2*number_of_digits;
+    int multiplier = 1;
+    int interval = number_of_digits-1;
     
-    for(int i=0; i<number_of_digits; i++)
+    while(interval >=0)
     {
-        intValue += binary_digits[i]*multiplier;
-        multiplier = multiplier/2;
+        intValue += binary_digits[interval]*multiplier;
+        multiplier = multiplier*2; 
+        interval = interval-1;
+
     }
 
     return intValue;
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-extern int binary_to_int(int binary_digits[], int number_of_digits);
+extern int binary_to_int(int binary_digits[30], int number_of_digits);
 
 int main()
 {
