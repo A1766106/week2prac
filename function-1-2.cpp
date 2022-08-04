e1e5fa9cd97832b3c4579a96c141405b7b610e2b
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-04 22:07:36 +0930

    cheer

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -9,11 +9,11 @@ int is_identity(int array[10][10])
     {
         for(int j = 0; j < 10; j++)
         {
-            if(array[i][j] != 1)
+            if(array[i][i] != 1)
             {
                 return 0;
             }
-            if(i != j && array[i][j] == 1)
+            if(i != j && array[i][j] != 0)
             {
                 return 0;    
             }
